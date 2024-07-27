2d16979d8254884a9e522aa2a73b10286bab0064
PrinceGamedev <155161699+PrinceGamedev@users.noreply.github.com>
2024-07-27 22:10:19 +0530

    Update

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: "/profile.png",
   designation: "Game Developer",
   description:
-    "My name is PRINCE SAVALIYA. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of game development and love to make the games more open to the world. My core skill is based on C# and I love to do most of the things using C#. I am available for any kind of job opportunity that suits my skills and interests.",
+    "My name is Prince Savaliya. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of game development and love to make the games more open to the world. My core skill is based on C# and I love to do most of the things using C#. I am available for any kind of job opportunity that suits my skills and interests.",
   email: "gamedeveloper.prince@gamil.com",
   phone: "+91-7383083023",
   address: "Surat, Gujarat, India (395010) ",
